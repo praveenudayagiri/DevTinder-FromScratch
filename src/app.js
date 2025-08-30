407a4280dcd8160d39b248f41dc7b18a6d3bc617
praveenudayagiri <praveenudayagiri724@gmail.com>
2025-08-30 06:04:07 +0530

    Delete, Patch API's on user

--- src/app.js
+++ src/app.js
@@ -20,6 +20,50 @@ app.post("/signup",async(req,res)=>{
     }
 })
 
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.email;
+    try{
+        const user = await User.find({email:userEmail});
+        res.send(user);
+    }
+    catch(err){
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(404).send("Error Fetching Users");
+    }
+})
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body._id;
+    try{
+        await User.deleteOne({_id:userId});
+        res.send("User deleted Sucessfully");
+    }
+    catch(err){
+        res.send(404).send("Error to delete user in Database");
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body._id;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("User Details Updated Sucessfully");
+    }
+    catch(err){
+        res.status(404).send("Error while updating user details");
+    }
+})
+
 connectDB().
 then(()=>{
     console.log("Database Connected Sucesssfully");
