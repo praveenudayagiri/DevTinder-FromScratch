12e3c6631a29beea7b34b5d7338d55830226efd4
praveenudayagiri <praveenudayagiri724@gmail.com>
2025-09-01 20:17:59 +0530

    /request/:status API Implementation & Indexing of Database

---
+++ config/connectionrequest.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+    },
+    toUserId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+    },
+    status: {
+        type: String,
+    validate(value) {
+    if (!["ignored", "interested", "accepted", "rejected"].includes(value)) {
+      throw new Error("Status data invalid");
+    }
+  },
+  required: true
+    },
+
+
+})
+
+
+connectionRequestSchema.index({fromUserId:1,toUserId:1});
+
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest = this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("You can't send Connection request to your self");
+    }
+    next();
+})
+
+module.exports = mongoose.model("ConnectionRequest",connectionRequestSchema);
--- routes/request.js
+++ routes/request.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const requestRouter = express.Router();
 const {isUserAuth} = require("../middlewares/auth");
+const ConnectionRequest = require("../config/connectionrequest");
 
-requestRouter.post("/sendrequest",isUserAuth,(req,res)=>{
-    res.send("Connection Send Sucessfull");
+
+requestRouter.post("/request/:status/:toUserId",isUserAuth,async(req,res)=>{
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const ALLOWED_STATUS = ["ignored","intrested"];
+        if(!ALLOWED_STATUS.includes(status)){
+            return res.send("Invalid Status Type");
+        }
+
+        const isAlreadyExist = await ConnectionRequest.findOne({
+            $or:[
+                {fromUserId,toUserId},
+                { fromUserId:toUserId,toUserId:fromUserId }
+            ],
+        })
+        if(isAlreadyExist){
+            return res.status(400).send("Connection Request Already Exist");
+        }
+
+        const newRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+        await newRequest.save();
+        res.send("New Connection Request Added Scucessfully");
+
+    }
+    catch(err){
+        res.status(400).send("Error Updating the Data: "+err.message);
+    }
+
+
 })
 
 
